1c6e8baadb818fba48017a2a450d43a98732b8ff
mohamedhamdy123890456@gmail.com <mohamedhamdy123890456@gmail.com>
2021-07-25 15:10:01 +0200

    /

--- test/controllers/controllers.py
+++ test/controllers/controllers.py
@@ -16,10 +16,13 @@ class Test(http.Controller):
         #      }}"""
         if kw.get('name', False):
             product=request.env['product.template'].sudo().search([('name','=',kw.get('name', False))])
-            if kw.get('price', 0) == product.list_price:
-                return False
+            if product:
+                if kw.get('price', 0) == product.list_price:
+                    return False
+                else:
+                    return True
             else:
-                return True
+                return "not Found"
 
 
 
